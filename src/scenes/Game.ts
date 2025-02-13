6bf2e64e98fb1e8db868596d67dffd4468aa2b16
TDBrody <brody.seyranian28@student.tamdistrict.org>
2025-02-13 22:55:58 +0000

    fgcgffhfg

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,52 +1,48 @@
 import { Scene } from 'phaser';
 
-export class Game extends Scene
-{
+export class Game extends Scene {
+    stars: Phaser.Physics.Arcade.Group;
+
     platforms: Phaser.Physics.Arcade.StaticGroup;
     player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
     cursors: any;
-    constructor ()
-    {
+    constructor() {
         super('Game');
     }
-    preload (){
+    preload() {
         this.load.image('sky', 'assets/sky.png');
         this.load.image('ground', 'assets/platform.png');
         this.load.image('star', 'assets/star.png');
         this.load.image('bomb', 'assets/bomb.png');
-        this.load.spritesheet('dude', 
+        this.load.spritesheet('dude',
             'assets/dude.png',
+
             { frameWidth: 32, frameHeight: 48 }
         );
     }
-    update (){
-        if (this.cursors.left.isDown)
-            {
-                this.player.setVelocityX(-160);
-            
-                this.player.anims.play('left', true);
-            }
-            else if (this.cursors.right.isDown)
-            {
-                this.player.setVelocityX(160);
-            
-                this.player.anims.play('right', true);
-            }
-            else
-            {
-                this.player.setVelocityX(0);
-            
-                this.player.anims.play('turn');
-            }
-            
-            if (this.cursors.up.isDown && this.player.body.touching.down)
-            {
-                this.player.setVelocityY(-330);
-            }
+    update() {
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160);
+
+            this.player.anims.play('left', true);
+        }
+        else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160);
+
+            this.player.anims.play('right', true);
+        }
+        else {
+            this.player.setVelocityX(0);
+
+            this.player.anims.play('turn');
+        }
+
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-330);
+        }
 
     }
-    create ()
-    {
+    create() {
         this.cursors = this.input.keyboard?.createCursorKeys();
         this.anims.create({
             key: 'left',
@@ -54,34 +50,49 @@ export class Game extends Scene
             frameRate: 10,
             repeat: -1
         });
-        
+
         this.anims.create({
             key: 'turn',
-            frames: [ { key: 'dude', frame: 4 } ],
+            frames: [{ key: 'dude', frame: 4 }],
             frameRate: 20
         });
-        
+
         this.anims.create({
             key: 'right',
             frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
             frameRate: 10,
             repeat: -1
         });
-        
+
         this.add.image(400, 300, 'sky');
-          this.add.image(400, 300, 'sky');
+        this.add.image(400, 300, 'sky');
 
-    this.platforms = this.physics.add.staticGroup();
+        this.platforms = this.physics.add.staticGroup();
 
-    this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-``
-    this.platforms.create(600, 400, 'ground');
-    this.platforms.create(50, 250, 'ground');
-    this.platforms.create(750, 220, 'ground');
-    this.player = this.physics.add.sprite(100, 450, 'dude');
-    this.physics.add.collider(this.player, this.platforms);
+        this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+        this.stars = this.physics.add.group({
+            key: 'star',
+            repeat: 11,
+            setXY: {
+                x: 12,
+                y: 0,
+                stepX: 70,
+            }
+        })
+        this.stars.children.iterate((star: any) => {
+            star.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+            return null;
+        })
 
-this.player.setBounce(0.2);
-this.player.setCollideWorldBounds(true);
+        this.physics.add.collider(this.stars, this.platforms);
+
+        this.platforms.create(600, 400, 'ground');
+        this.platforms.create(50, 250, 'ground');
+        this.platforms.create(750, 220, 'ground');
+        this.player = this.physics.add.sprite(100, 450, 'dude');
+        this.physics.add.collider(this.player, this.platforms);
+
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
     }
 }
