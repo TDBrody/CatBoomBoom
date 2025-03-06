cbd519e4c3a1aff5509885f1d00926e351cb8370
TDBrody <brody.seyranian28@student.tamdistrict.org>
2025-03-06 23:34:55 +0000

    Co-authored-by: Brandon Neuman <Brneuman27@users.noreply.github.com>

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -6,9 +6,12 @@ export class Game extends Scene {
     scoreText: any;
     meowmeowmeowcatmeow: any;
     gameOverImage: Phaser.GameObjects.Image;
-
+    intervalId: number;
+    happiCat: any;
+    catmood: any;
     constructor() {
         super('Game');
+       
     }
 
     preload() {
@@ -26,13 +29,19 @@ export class Game extends Scene {
         const character = this.add.sprite(856, 500, 'cat').setScale(0.75);
         character.setInteractive();
         character.setOrigin(0.5, 0.5); // Set the origin to the center
-        let Catnum = 0.8333;
-        console.log (Catnum);
+        let catNum = 0.8333;
+        let happiCat = 100;
+        setInterval(() => { 
+            happiCat -= catNum / 100;
+            console.log(`HappiCat: ${happiCat.toFixed(2)}`);
+            this.catmood.setText('Cat Happiness: ' + happiCat);
+          }, 10);
+        console.log (catNum);
         function convertToInversePercentage(decimal: number): string {
             return ((1 - decimal) * 100).toFixed(2) + '%';
         }
-        let Catsilly = convertToInversePercentage(Catnum);
-        console.log(Catsilly);
+        let catsilly = convertToInversePercentage(catNum);
+        console.log(catsilly);
         // Create the animations
         this.anims.create({
             key: 'click',
@@ -40,7 +49,7 @@ export class Game extends Scene {
             frameRate: 35, // Adjusted frame rate
             repeat: 0 // Play the animation once
         });
-
+        
         this.anims.create({
             key: 'boom',
             frames: this.anims.generateFrameNumbers('catboom', { start: 0, end: 9 }),
@@ -48,6 +57,30 @@ export class Game extends Scene {
             repeat: 0 // Play the animation once
         });
 
+
+          setInterval(() => {
+            if (happiCat < 40.20 && Math.random() < 0.1) {
+              happiCat += 5;
+              
+            }
+            if (happiCat < 70.69 && Math.random() < 0.01) {
+                happiCat += 5;
+                
+              }
+            if (Math.random() < 0.01) { 
+                happiCat -= 6.9;
+              }
+            if (Math.random() < 0.003) {
+                happiCat += 15;
+              }
+            if (happiCat > 99.99 && Math.random() < 0.1) {
+                happiCat -= 0.4;
+                
+              }
+            happiCat += happiCat + ((this.score)/10000)*-1;
+          }, 1);
+          
+       
         const meowSound = this.sound.add('meow'); // Add the audio to the scene
         const boooom = this.sound.add('meowboomboommeowboomy'); // Add the audio to the scene
 
@@ -55,18 +88,20 @@ export class Game extends Scene {
         this.gameOverImage = this.add.image(856, 460, 'gameOver').setScale(1.1);
         this.gameOverImage.setVisible(false);
         this.input.on('gameobjectup', (pointer, gameObject) => {
-            if (Math.random() < Catnum) {
+            if (Math.random() < catNum) {
                 gameObject.play('click');
-                Catnum -= 0.1667;
-                console.log(Catnum)
-                let Catsilly = convertToInversePercentage(Catnum);
-                console.log(Catsilly);
-                this.meowmeowmeowcatmeow.setText('Boom Boom Chance: ' + Catsilly);
+                catNum -= 0.1667;
+                console.log(catNum);
+                let catsilly = convertToInversePercentage(catNum);
+                console.log(catsilly);
+                happiCat += (catNum * 10) + 1; //Cat hug gratitude
+                this.meowmeowmeowcatmeow.setText('Boom Boom Chance: ' + catsilly);
             } else {
                 gameObject.play('boom');
                 boooom.play();
-                this.score += -100000;
+                this.score -= 100000;
                 gameObject.on('animationcomplete', () => {
+                   
                     this.gameOverImage.setVisible(true); // Make the game over image visible
                     this.input.enabled = false; // Disable further input
                 });
@@ -78,6 +113,7 @@ export class Game extends Scene {
 
         this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '48px', fill: '#000' });
         this.meowmeowmeowcatmeow = this.add.text(14, 50, 'Boom Boom Chance: 16.67%', { fontSize: '48px', fill: '#000' });
+        this.catmood = this.add.text(14, 90, 'Cat Happiness: 100', { fontSize: '48px', fill: '#000' });
         
     }
 
