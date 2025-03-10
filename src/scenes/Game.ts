210f0fdd885bcb7c2dc107738f75fe3e3de665fd
TDBrody <brody.seyranian28@student.tamdistrict.org>
2025-03-10 22:20:03 +0000

    meowmewmeowmeowmeowmeowmeowmeo

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -9,6 +9,8 @@ export class Game extends Scene {
     intervalId: number;
     happiCat: any;
     catmood: any;
+    catThink: any;
+    catThought: any;
     constructor() {
         super('Game');
        
@@ -77,7 +79,60 @@ export class Game extends Scene {
                 happiCat -= 0.4;
                 
               }
-            happiCat += happiCat + ((this.score)/10000)*-1;
+            happiCat += ((this.score)/100);
+            if (this.score < 0) {
+              happiCat = 0;
+              
+            }
+            {
+              if (happiCat < 0.1) {
+                this.catThought = "am deid :(";
+              } else if (happiCat >= 1 && happiCat <= 7) {
+                this.catThought = "am much depress";
+              } else if (happiCat > 7 && happiCat <= 14) {
+                this.catThought = "i am depres";
+              } else if (happiCat > 14 && happiCat <= 21) {
+                this.catThought = "so many sads";
+              } else if (happiCat > 21 && happiCat <= 28) {
+                this.catThought = "am the sad";
+              } else if (happiCat > 28 && happiCat <= 35) {
+                this.catThought = "no is the happy";
+              } else if (happiCat > 35 && happiCat <= 42) {
+                this.catThought = "sad is the me";
+              } else if (happiCat > 42 && happiCat <= 49) {
+                this.catThought = "am mid fr rn";
+              } else if (happiCat > 49 && happiCat <= 56) {
+                this.catThought = "am bored";
+              } else if (happiCat > 56 && happiCat <= 63) {
+                this.catThought = "am a cat";
+              } else if (happiCat > 63 && happiCat <= 70) {
+                this.catThought = "am a little sad but am ok";
+              } else if (happiCat > 70 && happiCat <= 77) {
+                this.catThought = "i want hug";
+              } else if (happiCat > 77 && happiCat <= 84) {
+                this.catThought = "am a ok";
+              } else if (happiCat > 84 && happiCat <= 91) {
+                this.catThought = "am happi";
+              } else if (happiCat > 91 && happiCat <= 98) {
+                this.catThought = "am so happy";
+              } else if (happiCat > 98 && happiCat <= 105) {
+                  this.catThought = "am the most not deressed";
+              } else if (happiCat > 105 && happiCat <= 112) {
+                this.catThought = "yippee";
+              } else if (happiCat > 112 && happiCat <= 119) {
+                this.catThought = "am eat cus am happy";
+              } else if (happiCat > 119 && happiCat <= 126) {
+                this.catThought = "me the happy so many";
+              } else if (happiCat > 126 && happiCat <= 133) {
+                this.catThought = "i dont wanna die";
+              } else if (happiCat > 133 && happiCat <= 140) {
+                this.catThought = "I'VE BEEN ENLIGHTENDED";
+              } else {
+                this.catThought = "am broken";
+              }
+              
+          }
+            this.catThink.setText(this.catThought);
           }, 1);
           
        
@@ -112,6 +167,7 @@ export class Game extends Scene {
         });
 
         this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '48px', fill: '#000' });
+        this.catThink = this.add.text(16, 140, '"I is a cat meow"', { fontSize: '48px', fill: '#000' });
         this.meowmeowmeowcatmeow = this.add.text(14, 50, 'Boom Boom Chance: 16.67%', { fontSize: '48px', fill: '#000' });
         this.catmood = this.add.text(14, 90, 'Cat Happiness: 100', { fontSize: '48px', fill: '#000' });
         
